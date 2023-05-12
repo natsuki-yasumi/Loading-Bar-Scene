6e6c08aa97680e60b7f00d06e89599b87be082f8
Feri Hermawan <ferihermawan007@gmail.com>
2023-05-12 23:11:08 +0700

    First Commit
    Ini adalah commit pertama

---
+++ Assets/LoaderScript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LoaderScript : MonoBehaviour
+{
+
+    public GameObject sceneLoad;
+    public Slider slider;
+
+    public void Loader(int sceneIndex)
+    {
+        StartCoroutine(LoadingScene(sceneIndex));
+    }
+
+    IEnumerator LoadingScene(int sceneIndex)
+    {
+        AsyncOperation proses =  SceneManager.LoadSceneAsync(sceneIndex);
+        sceneLoad.SetActive(true);
+
+        while(!proses.isDone)
+        {
+            float progress = Mathf.Clamp01(proses.progress / .9f);
+            slider.value = progress;
+            yield return null;
+        }
+    }
+}
